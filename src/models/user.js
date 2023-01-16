46afe79fffdf483656475fc9b3645d396736e263
KaiXuan Chin <47827034+kaixuan98@users.noreply.github.com>
2023-01-16 14:28:52 +0800

    Added user routes
    user routes include - login, register, logout and logout all

--- src/models/user.js
+++ src/models/user.js
@@ -43,5 +43,41 @@ const userSchema = new mongoose.Schema({
     timestamps: true // auto create a createdAt and updated at field in db
 })
 
+// the schema has this method - used to generate auth token
+userSchema.methods.generateAuthToken  = async function (){
+    const user = this; // this method will be bind with userSchema and will get access of all the instances with this keyword
+    const token = jwt.sign({ 
+        _id: user._id.toString()
+    }, process.env.JWT_SECRET);
+
+    user.tokens = [...user.tokens, token];
+    await user.save();
+    return token
+}
+
+// static function to fetch a user based on their email and password - for login purpose 
+userSchema.static.findByCredentials = async (email, password) => {
+    const user = await User.findOne({email});
+    if(!user){
+        throw new Error('No such user')
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch){
+        throw new Error('Incorrect Password')
+    }
+
+    return user
+}
+
+// pre middleware which run before a specific action 
+// this function will be run before we save the user 
+userSchema.pre('save', async function(next){
+    const user = this;
+    if(user.isModified('password')){
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+    next()
+})
+
 const User = mongoose.model('User', userSchema);
 module.exports = User
